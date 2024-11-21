58ef2f59871b060014ebd554748d3a263d8c46b8
Faith Etornam <faithgbadegbe1@gmail.com>
2024-11-21 13:49:16 +0000

    Ecommerce

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const connectToDB = require('./database/db');
+const authRoutes = require('./routes/auth-routes');
+
+const app = express();
+const PORT = process.env.PORT ;
+
+connectToDB();
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+}); 
